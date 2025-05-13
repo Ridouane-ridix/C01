8e9cbb71e848b7f4086ec9c166435a6b1f564424
Your Name <ridix0202@gmail.com>
2025-05-13 14:47:24 +0100

    C01

--- count_files.sh
+++ count_files.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 # Utilise le dossier courant
 Myfolder="."
-
 # Récupère le nom du dossier courant (nom simple, pas le chemin)
 NomDuDossier=$(basename "$PWD")
-
 # Affiche juste le nom du dossier
 echo "$NomDuDossier"
-
 # Compte les fichiers (en excluant les répertoires)
 count=$(ls -p "$Myfolder" | grep -v / | wc -l)
-
 # Affiche le nombre de fichiers
 echo "Le dossier $NomDuDossier contient $count fichier(s)."
