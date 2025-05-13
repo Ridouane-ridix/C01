1b5561b5c59d945b84c58f3b51aa8a6c71ad2bab
Your Name <ridix0202@gmail.com>
2025-05-13 14:29:34 +0100

    C01

--- count_files.sh
+++ count_files.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+
 # Utilise le dossier courant (.) comme cible
-Myfolder="."  
-# Liste les éléments du dossier avec ls -p :
-# -p ajoute un / à la fin des noms de répertoires
-# grep -v / permet d'exclure les répertoires
-# wc -l compte le nombre de fichiers
+Myfolder="."
+NomDuDossier=$(basename "$PWD")
 count=$(ls -p "$Myfolder" | grep -v / | wc -l)
 # Affiche le résultat
-echo "Le dossier myfolder contient $count fichier(s)."
+echo "Le dossier $NomDuDossier  contient $count fichier(s)."
+
 
