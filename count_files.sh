ed7e7dc007df35e952fccb760eeeec35ccbc813e
Your Name <ridix0202@gmail.com>
2025-05-14 19:18:37 +0100

    C01

--- count_files.sh
+++ count_files.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-
 # Demande à l'utilisateur de saisir le nom du dossier
-echo "Entrez le nom du dossier :"
 read Myfolder
-
 # Affiche le nom du dossier
 echo "$Myfolder"
-
-# Initialise le compteur
-count=0
-
-# Boucle sur les éléments listés dans le dossier
-for item in $(ls "$Myfolder"); do
-  # Vérifie si c’est un fichier (et pas un dossier)
-  if [ -f "$Myfolder/$item" ]; then
-    count=$((count + 1))
-  fi
-done
-
+#comptage des fichiers
+count=$(ls -1 "$Myfolder" | wc -l)
 # Affiche le résultat
 echo "Le dossier $Myfolder contient $count fichier(s)."
